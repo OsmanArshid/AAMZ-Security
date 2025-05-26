21df6f65cc049407fae457a791b9829e07398bb8
OsmanArshid <usman.arshid01@outlook.com>
2025-05-26 06:55:04 +0500

    Starting

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+                'custom-lg': '1100px', 
+      },
+    },
+  },
+  plugins: [],
+}
